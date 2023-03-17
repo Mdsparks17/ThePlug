02525362eb65aa798efa2372045793df2e5b265c
Mdsparks17 <56485515+Mdsparks17@users.noreply.github.com>
2023-03-17 09:54:43 -0700

    fixing bad css/html

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,19 +7,12 @@ import { Component, OnInit, HostListener  } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  isMobil: string = "mobile-align";
-
   constructor() {
-    this.getScreenSize();
    }
 
   ngOnInit(): void {
   }
 
-  @HostListener('window:resize', ['$event'])
-  getScreenSize() {
-    //  console.log(window.innerWidth);
-    window.innerWidth > 750 ? this.isMobil = "web-align" :  this.isMobil = "mobile-align";
-  }
+
 
 }
