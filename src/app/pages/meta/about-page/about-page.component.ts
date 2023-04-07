8e85a5c57eaee582ab00a7ad82aad3b1811af713
Michael Sparks <mdsparks17@gmail.com>
2023-04-07 09:40:54 -0700

    auto-commit

--- src/app/pages/meta/about-page/about-page.component.ts
+++ src/app/pages/meta/about-page/about-page.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
+  jpeg240 = "<a title=\"ThePlug\" href=\"https://theplug.icu/\"><img src=\"https://sizeof.cat/assets/images/theplug-240x60.jpg\" alt=\"ThePlug\" /></a>"
+  jpeg240inverse = "<a title=\"ThePlug\" href=\"https://theplug.icu/\"><img src=\"https://sizeof.cat/assets/images/theplug-240x60-inverse.jpg\" alt=\"ThePlug\" /></a>"
+  png240 = "<a title=\"ThePlug\" href=\"https://theplug.icu/\"><img src=\"https://sizeof.cat/assets/images/theplug-240x60.png\" alt=\"ThePlug\" /></a>"
+  png240inverse = "<a title=\"ThePlug\" href=\"https://theplug.icu/\"><img src=\"https://sizeof.cat/assets/images/theplug-240x60-inverse.png\" alt=\"ThePlug\" /></a>"
+  jpeg88 = "<a title=\"ThePlug\" href=\"https://theplug.icu/\"><img src=\"https://sizeof.cat/assets/images/theplug-88x31.jpg\" alt=\"ThePlug\" /></a>"
+  jpeg88inverse = "<a title=\"ThePlug\" href=\"https://theplug.icu/\"><img src=\"https://sizeof.cat/assets/images/theplug-88x31-inverse.jpg\" alt=\"ThePlug\" /></a>"
+
+
+
   constructor() { }
 
   ngOnInit(): void {
