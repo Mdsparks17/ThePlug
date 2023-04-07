5cfd1cefe75ea1dbe987e8114e5e5ea4127d5666
Michael Sparks <mdsparks17@gmail.com>
2023-04-07 11:03:38 -0700

    auto-commit

--- src/app/pages/projects/bingo-generator/bingo-generator.component.ts
+++ src/app/pages/projects/bingo-generator/bingo-generator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BingoGeneratorComponent implements OnInit {
   bingoCard = [
-    {topic: "temp", color: ""}
+    {topic: "", color: ""}
 
   ];
 
